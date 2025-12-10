dcf596dcf00ca6ad0dee25a663aeb0f05eb9af54
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2025-12-10 14:49:53 +0200

    Add basic FEM files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,57 @@
 function App() {
-  return <div className="bg-red-500">App</div>;
+  return (
+    <div className="bg-blue-200">
+      <p>Units</p>
+
+      <p>Switch to Imperial/Metric</p>
+
+      <p>Temperature</p>
+
+      <p>Celsius (°C)</p>
+      <p>Fahrenheit (°F)</p>
+
+      <p>Wind Speed</p>
+
+      <p>km/h</p>
+      <p>mph</p>
+
+      <p>Precipitation</p>
+
+      <p>Millimeters (mm)</p>
+      <p>Inches (in)</p>
+
+      <p>How's the sky looking today?</p>
+
+      <p>Search for a city, e.g., New York</p>
+      <p>Search</p>
+
+      <p>Feels like</p>
+      {/* Insert temperature here  */}
+
+      <p>Humidity</p>
+      {/* Insert humidity here  */}
+
+      <p>Wind</p>
+      {/* Insert wind here     */}
+
+      <p>Precipitation</p>
+      {/* Insert precipitation here  */}
+
+      <p>Daily forecast</p>
+      {/* Insert daily forecast for the next 7 days here  */}
+
+      <p>Hourly forecast</p>
+      {/* Insert hourly forecast for the selected day here  */}
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
 }
 
 export default App;
